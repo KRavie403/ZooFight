ebbc55a74008d2f6743cb0562b1cd24783ac3bf2
Zoe <83668266+KRavie403@users.noreply.github.com>
2024-03-30 23:04:34 +0900

    Modify LoadResultUI.cs

--- ZooFight-Project/Assets/02.Script/UISystems/LoadResultUI.cs
+++ ZooFight-Project/Assets/02.Script/UISystems/LoadResultUI.cs
@@ -1,8 +1,12 @@
 using UnityEngine;
 using UnityEngine.UI;
+using static HitScanner;
 
 public class LoadResultUI : MonoBehaviour
 {
+    //[SerializeField]
+    //HitScanner.Team DecisionTeam = HitScanner.Team.NotSetting;
+
     // Eff-카메라 조정
     public Camera UICamera;
     [SerializeField] private float _winFOV = 50f;
@@ -21,33 +25,42 @@ public class LoadResultUI : MonoBehaviour
     void Start()
     {
         LoadResultImg();
-        LoadUserName();
-        LoadEff();
+        //LoadUserName();
+        //LoadEff();
     }
 
-    void LoadResultImg()
+    void LoadResultImg(/*HitScanner.Team BeaconTeam*/)
     {
-        //If()    // 이겼을 경우
+        //HitScanner.Team playerTeam = GetPlayerTeam();   // 플레이어의 팀 정보 가져오기
+        //HitScanner.Team winningTeam = Gamemanager.instance.winningTeam;
+
+        //if (winningTeam == playerTeam)
+        ////if (GameManager.instance.winningTeam == HitScanner.Team.Red)
+        ////if(Gamemanager.Inst.WinnerTeam == HitScanner.Team.Red)    // 승리
+        ////if(DecisionTeam == BeaconTeam)    // 승리
         //{
-                _textSprite = Resources.Load<Sprite>("WIN");
-                _t1Sprite = Resources.Load<Sprite>("WIN(2)");
-                _t2Sprite = Resources.Load<Sprite>("LOSE(2)");
+        //    _textSprite = Resources.Load<Sprite>("WIN");
+        //    _t1Sprite = Resources.Load<Sprite>("WIN(2)");
+        //    _t2Sprite = Resources.Load<Sprite>("LOSE(2)");
         //}
-        //else if ()   // 졌을 경우
-        //{
-                //_textSprite = Resources.Load<Sprite>("LOSE");
-                //_t1Sprite = Resources.Load<Sprite>("WIN(3)");
-                //_t2Sprite = Resources.Load<Sprite>("LOSE(3)");
+        //else if (winningTeam != playerTeam && winningTeam != HitScanner.Team.NotSetting)
+        ////else if (winningTeam == HitScanner.Team.Blue)
+        ////else if (GameManager.instance.winningTeam == HitScanner.Team.Blue)
+        ////else if (Gamemanager.Inst.WinnerTeam == HitScanner.Team.Blue)   // 패배
+        ////else if (BeaconTeam == HitScanner.Team.NotSetting)   // 패배
+        //    _textSprite = Resources.Load<Sprite>("LOSE");
+        //    _t1Sprite = Resources.Load<Sprite>("WIN(3)");
+        //    _t2Sprite = Resources.Load<Sprite>("LOSE(3)");
         //}
         //else  // 비겼을 경우
         //{
-                //_textSprite = Resources.Load<Sprite>("DRAW");
-                //_t1Sprite = Resources.Load<Sprite>("DRAW(2)");
-                //_t2Sprite = Resources.Load<Sprite>("DRAW(2)");
+        //    _textSprite = Resources.Load<Sprite>("DRAW");
+        //    _t1Sprite = Resources.Load<Sprite>("DRAW(2)");
+        //    _t2Sprite = Resources.Load<Sprite>("DRAW(2)");
         //}
-            text.sprite = _textSprite;
-            t1.sprite = _t1Sprite;
-            t2.sprite = _t2Sprite;
+        //text.sprite = _textSprite;
+        //    t1.sprite = _t1Sprite;
+        //    t2.sprite = _t2Sprite;
     }
 
     void LoadUserName()
