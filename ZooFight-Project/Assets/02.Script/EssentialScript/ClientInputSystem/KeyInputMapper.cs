b5e98a527655cc617726381b83f4f27917836dd5
Zoe <83668266+KRavie403@users.noreply.github.com>
2024-10-14 19:26:52 +0900

    feat: 키 컨트롤러 수정

--- ZooFight-Project/Assets/02.Script/EssentialScript/ClientInputSystem/KeyInputMapper.cs
+++ ZooFight-Project/Assets/02.Script/EssentialScript/ClientInputSystem/KeyInputMapper.cs
@@ -10,25 +10,26 @@ using System.Linq;
 
 public enum KeyAction
 {
-    Forward=0,Backward,Left,Right,
-    Jump,Attack,Selectskill,Usingskill,Grab,
-    Menu,Run,
-    ItemCreate,    
+    Forward = 0, Backward, Left, Right,
+    Jump, Attack, Selectskill, Usingskill, Grab,
+    Menu, Run,
+    ItemCreate,
+    // 맵열기 = M
     KeyCount
 }
 
 public static class KeySetting
 {
-    public static Dictionary<KeyAction,KeyCode> keys = new Dictionary<KeyAction,KeyCode>();
+    public static Dictionary<KeyAction, KeyCode> keys = new Dictionary<KeyAction, KeyCode>();
 }
 
 
-public class KeyInputMapper : MonoBehaviour 
+public class KeyInputMapper : MonoBehaviour
 {
     KeyCode[] defaultKeys = new KeyCode[]
     {
-        KeyCode.W, KeyCode.S, KeyCode.A, KeyCode.D, 
-        KeyCode.Space, KeyCode.Mouse0, KeyCode.E, KeyCode.F,KeyCode.Mouse1, 
+        KeyCode.W, KeyCode.S, KeyCode.A, KeyCode.D,
+        KeyCode.Space, KeyCode.Mouse0, KeyCode.E, KeyCode.F,KeyCode.Mouse1,
         KeyCode.Escape ,KeyCode.LeftShift,
         KeyCode.Q
 
@@ -44,7 +45,7 @@ public class KeyInputMapper : MonoBehaviour
 
     }
 
-    
+
 
 
     // Start is called before the first frame update
@@ -59,14 +60,14 @@ public class KeyInputMapper : MonoBehaviour
     void Update()
     {
 
-        
 
 
-           
+
+
         if (Input.anyKeyDown)
         {
-            
-            
+
+
 
         }
 
@@ -75,11 +76,14 @@ public class KeyInputMapper : MonoBehaviour
 
     public void KeycodeToInt(string keycode)
     {
-       
+
 
 
     }
 
-
+    public void TargetKeySetting(KeyAction KeyType, KeyCode keyCode)
+    {
+        KeySetting.keys[KeyType] = keyCode;
+    }
 
 }
